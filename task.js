a279be235e190f9ae2c89c29ccfe4dcaff963439
ÐSergey Ivanov <director51460mr@gmail.com>
2022-11-13 19:42:37 +0300

    save

--- task.js
+++ task.js
@@ -3,14 +3,11 @@ function solveEquation(a, b, c) {
     let arr = [];
     let Discriminant = Math.pow(b, 2)-4*a*c;
     let root = -b/(2*a);
-
-    if (Discriminant < 0) {
-        console.log('No roots, empty array');
-    } if (Discriminant = 0) {
-        console.log( `Root 1 ${root}`);
+    if (Discriminant === 0) {
+        return root;
     } else {
-        arr.push(-b + Math.sqrt(Discriminant) )/(2*a); 
-        arr.push(-b - Math.sqrt(Discriminant) )/(2*a);
+        arr.push((-b + Math.sqrt(Discriminant) )/(2*a)); 
+        arr.push((-b - Math.sqrt(Discriminant) )/(2*a));
     }
     return arr; // array
   }
