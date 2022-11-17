f2e507b2003a5f47e546932103163c2c22f56184
ÐSergey Ivanov <director51460mr@gmail.com>
2022-11-17 10:30:02 +0300

    end

--- task.js
+++ task.js
@@ -2,15 +2,14 @@
 function solveEquation(a, b, c) {
     let arr = [];
     let Discriminant = Math.pow(b, 2)-4*a*c;
-    if (Discriminant === 0) {
-        arr.push(-b/(2*a));
+    console.log(Discriminant);
+    if (Discriminant == 0) {
+      arr.push(-b/(2*a));
+    } else if (Discriminant > 0) {
+      arr.push((-b + Math.sqrt(Discriminant) )/(2*a)); 
+      arr.push((-b - Math.sqrt(Discriminant) )/(2*a));
     } else {
-        if (Discriminant > 0){
-          console.log("Not root");
-        } else {
-          arr.push((-b + Math.sqrt(Discriminant) )/(2*a)); 
-          arr.push((-b - Math.sqrt(Discriminant) )/(2*a));
-        }
+      console.log("Not root"); 
     }
     return arr; // array
   }
